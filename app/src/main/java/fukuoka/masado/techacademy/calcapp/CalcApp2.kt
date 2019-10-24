c8f2221120f1f8e5302cb816601bce782b6eef03
Masado Fukuoka <masado@eurovision.co.jp>
2019-10-24 17:39:15 +0900

    3回目のコミット

--- app/src/main/java/fukuoka/masado/techacademy/calcapp/CalcApp2.kt
+++ app/src/main/java/fukuoka/masado/techacademy/calcapp/CalcApp2.kt
@@ -2,6 +2,7 @@ package fukuoka.masado.techacademy.calcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_calc_app2.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
@@ -13,23 +14,30 @@ class CalcApp2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calc_app2)
 
-        val value1 = intent.getIntExtra("VALUE1", 0)
-        val value2 = intent.getIntExtra("VALUE2", 0)
+        val value1 = intent.getDoubleExtra("VALUE1", 0.00)
+        val value2 = intent.getDoubleExtra("VALUE2", 0.00)
         textView1.text = "足し算結果: ${value1 + value2}"
 
-        val value3 = intent.getIntExtra("VALUE3",0)
-        val value4 = intent.getIntExtra("VALUE4", 0)
+        val value3 = intent.getDoubleExtra("VALUE3",0.00)
+        val value4 = intent.getDoubleExtra("VALUE4", 0.00)
         textView2.text = "引き算結果: ${value3 - value4}"
 
-        val value5 = intent.getIntExtra("VALUE5",0)
-        val value6 = intent.getIntExtra("VALUE6", 0)
+        val value5 = intent.getDoubleExtra("VALUE5",0.00)
+        val value6 = intent.getDoubleExtra("VALUE6", 0.00)
         textView3.text = "掛け算結果: ${value5 * value6}"
 
-        val value7 = intent.getIntExtra("VALUE7",0)
-        val value8 = intent.getIntExtra("VALUE8", 0)
-        textView4.text = "割り算結果: ${value7 / value8}"
+        val value7 = intent.getDoubleExtra("VALUE7",0.00)
+        val value8 = intent.getDoubleExtra("VALUE8", 0.00)
+
+        if(value7 > -1 && value7 < 1 || value8 > -1 && value8 < 1) {
+            textView4.text = "割り算結果:ゼロ以外の数字で計算してください。"
+        }else {
+            textView4.text = "割り算結果: ${value7 / value8}"
+        }
+
+
+
 
     }
-
-
 }
+
